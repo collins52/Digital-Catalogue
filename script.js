cd21bb622dedd05c57ceaf66809c373f9fc37390
collins52 <collinsomonievusa@gmail.com>
2024-12-06 21:36:00 +0100

    uploaded files

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('Hello world') //testing if javascripts correctly connected
+
+//display drop down
+const categoryItems = document.getElementById('categoryItems')
+const category = document.getElementById('category');
+category.addEventListener('click', function(){
+    console.log('detected click')
+})
