124eccab75d7dc5401c006cae6d11a671e2e80cb
Collins Omonievusa <collinsomonievusa@gmail.com>
2024-12-10 03:07:18 +0100

    Added functions to menu button and close button to open and close side nav bar

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-console.log('Hello world') //testing if javascripts correctly connected
-
 //display drop down
 const categoryItems = document.getElementById('categoryItems')
 const category = document.getElementById('category');
-category.addEventListener('click', function(){
-    console.log('detected click')
+const body = document.body
+const navBar = document.getElementById('nav')
+
+body.addEventListener('click', function(evt){
+    const event = evt.target;
+
+    // Menu Button
+    if(event.className.includes('menu-bar')){
+        if(navBar.style.width == ''){
+            navBar.style.width = '64vw'
+        }
+        
+    }else if(event.id.includes('close')){
+        console.log('click detected')
+        navBar.style.width = '';
+    }else if(navBar.style.width == '64vw'){
+        console.log('width = 64vw')
+        if(!event.id.includes('nav')){
+            navBar.style.width = ''
+        }
+    }
 })
